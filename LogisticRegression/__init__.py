23afd8d185fad0fa59d9fe7c39cc92dc379fae6e
TuringEmmy <yonglonggeng@163.com>
2020-09-09 19:16:29 +0800

    boost

---
+++ LogisticRegression/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/9/9-17:43
+# @Author   : TuringEmmy
+# @Email    : yonglonggeng@163.com
+# @WeChat   : csy_lgy
+# @File     : __init__.py.py
+# @Project  : Sep-Dragon
+# *************************************************

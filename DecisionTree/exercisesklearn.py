c1b62e1e668763b52f36c3f18c9c126044115b33
TuringEmmy <yonglonggeng@163.com>
2020-09-09 17:21:19 +0800

    sklearn in tree

---
+++ DecisionTree/exercisesklearn.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2020/9/9-17:14
+# @Author   : TuringEmmy
+# @Email    : yonglonggeng@163.com
+# @WeChat   : csy_lgy
+# @File     : exercisesklearn.py
+# @Project  : Sep-Dragon
+# *************************************************
+# data
+import graphviz
+import numpy as np
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+
+
+def create_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_names)
+    df['label'] = iris.target
+    df.columns = [
+        'sepal length', 'sepal width', 'petal length', 'petal width', 'label'
+    ]
+    data = np.array(df.iloc[:100, [0, 1, -1]])
+    # print(data)
+    return data[:, :2], data[:, -1]
+
+
+if __name__ == '__main__':
+    X, y = create_data()
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    clf = DecisionTreeClassifier()
+    clf.fit(X_train, y_train, )
+    res = clf.score(X_test, y_test)
+    print(res)
+    tree_pic = export_graphviz(clf, out_file="mytree.pdf")
+    with open('mytree.pdf') as f:
+        dot_graph = f.read()
+
+    graphviz.Source(dot_graph)
